19ad0836f75ad4ab071458fe7464047b0244d014
Jose Juan Montiel <jose@debian>
2014-05-07 00:39:06 +0200

    Initial structure

---
+++ slides/What's Happening.mm
@@ -0,0 +1,82 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="What&apos;s Happening" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1399412874356"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5"/>
+<node TEXT="Yo" POSITION="right" ID="ID_924422532" CREATED="1399413806130" MODIFIED="1399413808399">
+<edge COLOR="#0000ff"/>
+<node TEXT="@accreativos" ID="ID_1866923962" CREATED="1399413817398" MODIFIED="1399413821095"/>
+<node TEXT="MadridGUG" ID="ID_1278479217" CREATED="1399413753810" MODIFIED="1399413836752">
+<node TEXT="Grupo de usuarios de Groovy" ID="ID_1898223432" CREATED="1399413774670" MODIFIED="1399413782474"/>
+<node TEXT="Como ayudar a la comunidad en Madrid en este caso" ID="ID_500736524" CREATED="1399413785310" MODIFIED="1399413802479"/>
+</node>
+</node>
+<node TEXT="Problema" POSITION="right" ID="ID_96460674" CREATED="1399413828187" MODIFIED="1399413850852">
+<edge COLOR="#00ff00"/>
+<node TEXT="WhatsHappening" ID="ID_1475508722" CREATED="1399413854060" MODIFIED="1399413859510">
+<node TEXT="https://github.com/josejuanmontiel/whatshappening" ID="ID_1814924623" CREATED="1399413890111" MODIFIED="1399413890111" LINK="https://github.com/josejuanmontiel/whatshappening"/>
+<node TEXT="Quieres medir como de rapido viaja una imagen en un momento determinado: derrota del Barca, Aguirre a la carrera..." ID="ID_1473556229" CREATED="1399413903764" MODIFIED="1399414077663"/>
+</node>
+</node>
+<node TEXT="Solucion" POSITION="right" ID="ID_1376484677" CREATED="1399414082864" MODIFIED="1399414087781">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Tecnologia" ID="ID_1026697642" CREATED="1399414090782" MODIFIED="1399414097057">
+<node TEXT="Dropwizard" ID="ID_710779118" CREATED="1399414099350" MODIFIED="1399414105737"/>
+<node TEXT="Groovy" ID="ID_587897482" CREATED="1399414107526" MODIFIED="1399414168755">
+<node TEXT="DSL" ID="ID_936806254" CREATED="1399414171063" MODIFIED="1399414173345"/>
+</node>
+</node>
+</node>
+<node TEXT="Para esta presentacion" POSITION="right" ID="ID_557388051" CREATED="1399414213375" MODIFIED="1399414220503">
+<edge COLOR="#00ffff"/>
+<node TEXT="http://berteh.github.io/mindslide/" ID="ID_1763940128" CREATED="1399414289174" MODIFIED="1399414289174" LINK="http://berteh.github.io/mindslide/"/>
+</node>
+</node>
+</map>
