8edb0284e9177f1e54da02ca9e73b5fc1f203df1
Jose Juan Montiel <josejuan.montiel@tecnilogica.com>
2014-05-29 14:45:00 +0200

    Cambios en la manera de copiar el fichero...
    Signed-off-by: Jose Juan Montiel <josejuan.montiel@tecnilogica.com>

--- server/core/src/main/java/com/accreativos/whatshappening/resources/WhatsHappeningFileResource.java
+++ server/core/src/main/java/com/accreativos/whatshappening/resources/WhatsHappeningFileResource.java
@@ -1,23 +1,27 @@
 package com.accreativos.whatshappening.resources;
 
-import com.accreativos.whatshappening.core.Saying;
-import com.google.common.base.Optional;
-import com.codahale.metrics.annotation.Timed;
-
-import com.google.common.io.Files;
-import com.google.common.io.InputSupplier;
-import com.sun.jersey.multipart.FormDataParam;
-import com.sun.jersey.core.header.FormDataContentDisposition;
+import static javax.ws.rs.core.MediaType.MULTIPART_FORM_DATA;
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.channels.FileChannel;
 import java.util.UUID;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import static javax.ws.rs.core.MediaType.*;
+
+import com.google.common.io.Files;
+import com.google.common.io.InputSupplier;
+import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataParam;
 
 @Path("/image")
 @Produces(MediaType.APPLICATION_JSON)
@@ -25,25 +29,43 @@ public class WhatsHappeningFileResource {
 
 	public static final String UPLOAD_DIR = "/tmp";
 
-    public WhatsHappeningFileResource() {
-    }
+	public WhatsHappeningFileResource() {
+	}
 
 	@POST
 	@Path("/attached")
-    @Produces(TEXT_PLAIN)
-    @Consumes(MULTIPART_FORM_DATA)
-    public String uploadFile(@FormDataParam("image") final InputStream stream,  @FormDataParam("image") FormDataContentDisposition fileDetail) throws Exception {
+	@Produces(TEXT_PLAIN)
+	@Consumes(MULTIPART_FORM_DATA)
+	public String uploadFile(@FormDataParam("image") final InputStream stream,
+			@FormDataParam("image") FormDataContentDisposition fileDetail)
+			throws Exception {
 
-        String tempname = UUID.randomUUID().toString();
-        final String outputPath = UPLOAD_DIR + File.separator + tempname;
-        Files.copy(new InputSupplier<InputStream>() {
-            @Override
-            public InputStream getInput() throws IOException {
-                return stream;
-            }
-        }, new File(outputPath));
+		String tempname = UUID.randomUUID().toString();
+		final String outputPath = UPLOAD_DIR + File.separator + tempname;
 
-        return tempname;
-    }
+		copyCompletely(stream, new FileOutputStream(outputPath));
+
+		return tempname;
+	}
+
+	public static void copyCompletely(InputStream input, OutputStream output)
+			throws IOException {
+		// if both are file streams, use channel IO
+		if ((output instanceof FileOutputStream)
+				&& (input instanceof FileInputStream)) {
+			try {
+				FileChannel target = ((FileOutputStream) output).getChannel();
+				FileChannel source = ((FileInputStream) input).getChannel();
+
+				source.transferTo(0, Integer.MAX_VALUE, target);
+
+				source.close();
+				target.close();
+
+				return;
+			} catch (Exception e) { /* failover to byte stream version */
+			}
+		}
+	}
 
 }
