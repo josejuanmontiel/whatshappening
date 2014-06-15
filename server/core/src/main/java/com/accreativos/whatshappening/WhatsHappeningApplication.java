5fbbf0aeda4705af52141e042ceff09b62cd9fb7
Jose Juan Montiel <josejuan.montiel@tecnilogica.com>
2014-06-15 19:31:37 +0200

    Registramos el factorydb

--- server/core/src/main/java/com/accreativos/whatshappening/WhatsHappeningApplication.java
+++ server/core/src/main/java/com/accreativos/whatshappening/WhatsHappeningApplication.java
@@ -1,10 +1,16 @@
 package com.accreativos.whatshappening;
 
 import io.dropwizard.Application;
+import io.dropwizard.jdbi.DBIFactory;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
-import com.accreativos.whatshappening.resources.*;
+
+import org.skife.jdbi.v2.DBI;
+
+import com.accreativos.whatshappening.db.FileDAO;
 import com.accreativos.whatshappening.health.TemplateHealthCheck;
+import com.accreativos.whatshappening.resources.WhatsHappeningFileResource;
+import com.accreativos.whatshappening.resources.WhatsHappeningResource;
 
 public class WhatsHappeningApplication extends Application<WhatsHappeningConfiguration> {
 
@@ -26,12 +32,23 @@ public class WhatsHappeningApplication extends Application<WhatsHappeningConfigu
     public void run(WhatsHappeningConfiguration configuration, Environment environment) {
 		final WhatsHappeningResource resource = new WhatsHappeningResource(configuration.getTemplate(),configuration.getDefaultName());
 	    environment.jersey().register(resource);
-		final WhatsHappeningFileResource resourceFile = new WhatsHappeningFileResource();
-	    environment.jersey().register(resourceFile);
 
+	    final DBIFactory factory = new DBIFactory();
+	    FileDAO dao = null;
+		try {
+			DBI jdbi = factory.build(environment, configuration.getDataSourceFactory(), "postgresql");
+		    dao = jdbi.onDemand(FileDAO.class);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	    
+	    final WhatsHappeningFileResource resourceFile = new WhatsHappeningFileResource(dao);
+	    environment.jersey().register(resourceFile);
 
 	    final TemplateHealthCheck healthCheck = new TemplateHealthCheck(configuration.getTemplate());
 	    environment.healthChecks().register("template", healthCheck);
+
+    
     }
 
 }
