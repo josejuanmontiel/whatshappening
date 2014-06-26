2f18c2c5eb2fcdbcb638f66540c9071589832ff2
Jose Juan Montiel <josejuan.montiel@tecnilogica.com>
2014-06-27 01:22:14 +0200

    Ajustamos las urls a pro.

--- client/android/whatshappening/src/main/java/com/mnm/asynctaskmanager/core/Task.java
+++ client/android/whatshappening/src/main/java/com/mnm/asynctaskmanager/core/Task.java
@@ -108,7 +108,7 @@ public final class Task extends AsyncTask<Void, String, String> {
 		String lineEnd = "\r\n";
 		String twoHyphens = "--";
 		String boundary = "*****";
-		String urlString = "http://192.168.1.152:9090/api/v1/image/upload";
+		String urlString = "http://whatshappening.no-ip.org/api/v1/image/upload";
 
 		try {
 			// ------------------ CLIENT REQUEST
